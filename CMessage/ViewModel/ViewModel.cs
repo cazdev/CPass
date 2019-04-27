2ced5c71fabd48698e7618b20662b848237d4a46
tavvi <tavvi.yt@gmail.com>
2019-04-27 19:32:51 +0300

    Added unit tests, refactoring

---
+++ CMessage/ViewModel/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPass.ViewModel
+{
+    public class ViewModel
+    {
+        public bool AddNew = false;
+        public bool removeBool = true;
+        public bool wipeBool = false;
+        public bool settingsBool = false;
+        public bool switchLoading = false;
+        public bool copyBool = false;
+    }
+}
---
+++ CPassTests/Model/StringCipherTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CMessage.Tests
+{
+    [TestClass()]
+    public class StringCipherTests
+    {
+        private readonly Random rnd = new Random();
+        [TestMethod()]
+        public void EncryptDecryptTest()
+        {
+            string clearText = CreateRandomString();
+            string pass = CreateRandomString();
+
+            string encText = StringCipher.Encrypt(clearText, pass);
+            string decText = StringCipher.Decrypt(encText, pass);
+
+            Assert.AreEqual(clearText, decText);
+        }
+
+        private string CreateRandomString()
+        {
+            string result = "";
+            for (int i = 0; i < rnd.Next(100, 200); i++)
+            {
+                result += (char)rnd.Next(40, 119);
+            }
+            return result;
+        }
+    }
+}
